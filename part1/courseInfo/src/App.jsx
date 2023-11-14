2c4672502b694fd18c03a367ea3aff0d83b6c4cc
Diogo Alhada <alhada06@gmail.com>
2023-11-14 18:45:05 +0000

    exercise 1.5

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 };
 
 const Content = (props) => {
@@ -55,27 +55,45 @@ const App = () => {
   //   exercises: 14,
   // };
   //-- ex1.4--
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  // const course = "Half Stack application development";
+  // const parts = [
+  //   {
+  //     name: "Fundamentals of React",
+  //     exercises: 10,
+  //   },
+  //   {
+  //     name: "Using props to pass data",
+  //     exercises: 7,
+  //   },
+  //   {
+  //     name: "State of a component",
+  //     exercises: 14,
+  //   },
+  // ];
+  //ex1.5
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
