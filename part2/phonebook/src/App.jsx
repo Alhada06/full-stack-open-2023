fd648ed95a18ffdd5d504f416dc5f61c0e684c53
Diogo Alhada <alhada06@gmail.com>
2023-11-16 21:55:40 +0000

    ex  2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchParam, setSearchParam] = useState("");
+  const [searching, setSearching] = useState(false);
+
   const handleNewNameChange = (event) => {
     setNewName(event.target.value);
   };
   const handleNewNumberChange = (event) => {
     setNewNumber(event.target.value);
+  };
+  const handleNewSearch = (event) => {
+    setSearchParam(event.target.value);
+    if (searchParam) {
+      setSearching(true);
+    } else {
+      setSearching(false);
+    }
   };
   const addPerson = (event) => {
     event.preventDefault();
@@ -28,9 +42,23 @@ const App = () => {
       setNewNumber("");
     }
   };
+
+  const search = () => {
+    return persons.filter((person) =>
+      person.name.toLowerCase().includes(searchParam)
+    );
+  };
+  const phonebook = searching ? search() : persons;
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with{" "}
+        <input onChange={handleNewSearch} value={searchParam} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={handleNewNameChange} value={newName} />
@@ -44,7 +72,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
 
-      {persons.map((person, i) => (
+      {phonebook.map((person, i) => (
         <p key={i}>
           {person.name}:{person.number}
         </p>
