181f4742dfd2287eeb0f5b6890752b6bc074f376
Diogo Alhada <alhada06@gmail.com>
2023-11-17 02:13:56 +0000

    ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ handleNewSearch, searchParam }) => {
   return (
@@ -34,16 +35,17 @@ const Persons = ({ persons }) =>
     </p>
   ));
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchParam, setSearchParam] = useState("");
   const [searching, setSearching] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  });
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value);
