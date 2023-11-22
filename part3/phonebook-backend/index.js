0595cea8b82367d750d3e0a32fbde3a4686affb3
Diogo Alhada <alhada06@gmail.com>
2023-11-22 16:38:26 +0000

    ex 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -39,6 +39,17 @@ app.get("/info", (request, response) => {
     } people</p><p>${new Date()}</p>`
   );
 });
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.statusMessage = "person's id does not exist";
+    response.status(404).end();
+  }
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
